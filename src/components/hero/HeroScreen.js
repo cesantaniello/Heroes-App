c0010efae06b2b11ea9fffb76b9d59abfca1953f
Administrator <admin@example.com>
2022-01-18 12:26:50 +0100

    Lee argumentos por URL

--- src/components/hero/HeroScreen.js
+++ src/components/hero/HeroScreen.js
@@ -1,7 +1,19 @@
+import { useParams, Navigate } from "react-router-dom";
+import { getHeroById } from "../../selectors/getHeroById";
+
 export const HeroScreen = () => {
+
+    const {heroeId} = useParams();
+    const hero = getHeroById(heroeId);
+
+    if (!hero) {
+        return <Navigate to="/"/>
+    };
+
     return (
         <div>
-            <h1>Hero Screen</h1>            
+            <h1>Hero Screen</h1>
+            <p>{hero.superhero}</p>            
         </div>
     )
 }
