fc449c78132b4cef32966069266cc5a3399adedd
Administrator <admin@example.com>
2022-01-19 13:05:19 +0100

    Aplica filtro de Heroes con QueryString

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,19 +1,27 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import queryString from "query-string";
+
 import { useForm } from "../../hooks/useForm";
 import { getHeroesByName } from "../../selectors/getHeroesByName";
 import { HeroCard } from "../hero/HeroCard";
 
 export const SearchScreen = () => {
 
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const { q = '' } = queryString.parse(location.search);
+
     const [formValues, handleInputChange] = useForm({
-        searchText: '',
+        searchText: q,
     });
 
     const { searchText } = formValues;
-    const heroesFiltered = getHeroesByName('');
+    const heroesFiltered = getHeroesByName(q);
 
     const handleSearch = (e) => {
         e.preventDefault();
-        console.log(searchText)
+        navigate(`?q=${searchText}`);
     }
 
     return (
