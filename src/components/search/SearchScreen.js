d4da868092a80822217aa515196c08543ea9f06f
Administrator <admin@example.com>
2022-01-19 12:12:38 +0100

    Agrega SearchComponent

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,7 +1,49 @@
+import { useForm } from "../../hooks/useForm";
+
 export const SearchScreen = () => {
+
+    const [formValues, handleInputChange] = useForm({
+        searchText: '',
+    });
+
+    const { searchText } = formValues;
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(searchText)
+    }
+
     return (
-        <div>
-            <h1>SearchScreen</h1>
-        </div>
+        <>
+            <h1>Search a hero</h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-5">
+                    <h4>Search</h4>
+                    <hr />
+
+                    <form 
+                        className="d-grid gap-2"
+                        onSubmit={handleSearch}>
+                        <input 
+                            type="text"
+                            placeholder="Search a hero"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                            value={searchText}
+                            onChange={handleInputChange}
+                        />
+
+                        <button 
+                            className="btn btn-primary mt-3"
+                            type="submit">
+                            Search...
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </>
     )
 }
