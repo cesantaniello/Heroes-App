bc543a88fbb6b81c56839fe04904434abf172d84
Administrator <admin@example.com>
2022-01-19 18:43:49 +0100

    Crea ruta privada para autenticacion

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,13 +1,19 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<LoginScreen />} />
-                <Route path="/*" element={<DashboardRoutes />} />
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <DashboardRoutes />
+                    </PrivateRoute>
+                    } 
+                />
             </Routes>
         </BrowserRouter>
     )
