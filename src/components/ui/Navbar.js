9778a32a53a7e75b0f9352db151b09ad049cc22f
Administrator <admin@example.com>
2022-01-20 16:00:37 +0100

    Arregla vista de hipervinculo en Navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,17 +1,12 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import { Link, NavLink, useNavigate } from 'react-router-dom'
-import { AuthContext } from '../../auth/authContext';
-import { types } from '../../types/types';
+
 
 export const Navbar = () => {
 
-    const {user, dispatch} = useContext(AuthContext);
     const navigate = useNavigate();
 
     const handleLogout = () => {
-
-        dispatch({type: types.logout});
-
         navigate('/login', {
             replace: true
         });
@@ -24,32 +19,32 @@ export const Navbar = () => {
                 className="navbar-brand" 
                 to="/"
             >
-                Asociaciones
+                Heroes App
             </Link>
 
             <div className="navbar-collapse">
                 <div className="navbar-nav">
 
                     <NavLink 
-                        className={ ({ isActive }) => 'nav-item nav-link' + (isActive ? 'active' : '' )} 
+                        className={ ({ isActive }) => 'nav-item nav-link ' + (isActive ? 'active' : '') }
                         to="/marvel"
                     >
                         Marvel
                     </NavLink>
 
                     <NavLink 
-                        className={ ({ isActive }) => 'nav-item nav-link' + (isActive ? 'active' : '' )} 
+                        className={ ({ isActive }) => 'nav-item nav-link ' + (isActive ? 'active' : '') }
                         to="/dc"
                     >
                         DC
                     </NavLink>
 
                     <NavLink 
-                        className={ ({ isActive }) => 'nav-item nav-link' + (isActive ? 'active' : '' )} 
+                        className={ ({ isActive }) => 'nav-item nav-link ' + (isActive ? 'active' : '') }
                         to="/search"
                     >
                         Search
-                    </NavLink>                    
+                    </NavLink>
                 </div>
             </div>
 
@@ -57,12 +52,12 @@ export const Navbar = () => {
                 <ul className="navbar-nav ml-auto">
 
                     <span className="nav-item nav-link text-info">
-                        {user.name}
+                        Carlos
                     </span>
-
+                    
                     <button 
-                        className="nav-item nav-link btn"
-                        onClick={handleLogout}
+                        className="nav-item nav-link btn" 
+                        onClick={ handleLogout }
                     >
                         Logout
                     </button>
