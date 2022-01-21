333c973bf86a5d7832d1561cc95f33993f9876f1
Administrator <admin@example.com>
2022-01-21 17:37:20 +0100

    Integra prueba de usuario autenticado en AppRouter

--- src/test/routers/AppRouter.test.js
+++ src/test/routers/AppRouter.test.js
@@ -4,17 +4,40 @@ import { AppRouter } from "../../routers/AppRouter";
 
 describe('Tests on <AppRouter />', () => {
 
-    const contextValue = {
-        user: {logged: false}
-    }
-
+    
     test('Should show a login if the user is not authenticated', () => {
+
+        const contextValue = {
+            user: {logged: false}
+        }
+
         const wrapper = mount(
             <AuthContext.Provider value={contextValue}>
                 <AppRouter />
             </AuthContext.Provider>
         );
+
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.find('h1').text().trim()).toBe('Login');
     })
+
+    test('Should show Marvel component if the user is authenticated', () => {
+
+        const contextValue = {
+            user: {
+                logged: true,
+                name: 'Carlos'
+            }
+        }
+
+        const wrapper = mount(
+            <AuthContext.Provider value={contextValue}>
+                <AppRouter />
+            </AuthContext.Provider>
+        );
+
+        // console.log(wrapper.html());
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.navbar').exists()).toBe(true);
+    })    
 })
