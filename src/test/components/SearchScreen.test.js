97261f85184061bb493f41c22733b055c3f6ae8c
Administrator <admin@example.com>
2022-01-22 12:24:34 +0100

    Integra pruebas con queryParameters

--- src/test/components/SearchScreen.test.js
+++ src/test/components/SearchScreen.test.js
@@ -13,4 +13,23 @@ describe('Tests on <SearchScreen />', () => {
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.find('.alert-info').text().trim()).toBe('Search a hero')
     })
+
+    test('Should show Batman and an input with queryString value', () => {
+        const wrapper = mount(
+        <MemoryRouter initialEntries={['/search?q=batman']}>
+            <SearchScreen />
+        </MemoryRouter>
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('input').prop('value')).toBe('batman')
+    })
+
+    test('Should show an error if hero is not found', () => {
+        const wrapper = mount(
+        <MemoryRouter initialEntries={['/search?q=batman123']}>
+            <SearchScreen />
+        </MemoryRouter>
+        );
+        expect(wrapper.find('.alert-danger').text().trim()).toBe('No results: batman123')
+    })
 })
