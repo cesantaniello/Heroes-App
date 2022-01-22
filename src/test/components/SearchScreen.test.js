456568ebcd906e40716afd1e52c4f16b36847ff8
Administrator <admin@example.com>
2022-01-22 13:02:17 +0100

    Agrega mock de funciones y prueba con requireActual

--- src/test/components/SearchScreen.test.js
+++ src/test/components/SearchScreen.test.js
@@ -2,6 +2,11 @@ import { mount } from "enzyme";
 import { MemoryRouter } from "react-router-dom";
 import { SearchScreen } from "../../components/search/SearchScreen";
 
+const mockNavigate = jest.fn();
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useNavigate: () => mockNavigate,
+}));
 
 describe('Tests on <SearchScreen />', () => {
     test('Should show correctly with default values', () => {
@@ -31,5 +36,26 @@ describe('Tests on <SearchScreen />', () => {
         </MemoryRouter>
         );
         expect(wrapper.find('.alert-danger').text().trim()).toBe('No results: batman123')
+    })
+
+    test('Should call navigate to new screen', () => {
+        const wrapper = mount(
+        <MemoryRouter initialEntries={['/search']}>
+            <SearchScreen />
+        </MemoryRouter>
+        );
+
+        wrapper.find('input').simulate('change', 
+            { target: {
+                name: 'searchText',
+                value: 'batman' 
+            } 
+        });
+
+        wrapper.find('form').prop('onSubmit')({
+            preventDefault(){}
+        })
+
+        expect(mockNavigate).toHaveBeenCalledWith('?q=batman');
     })
 })
